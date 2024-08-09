2f6cc764b79a37ed1e64fa31157a94d302cb6ad0
PARK SEONGJONG <48579953+SEONZONE@users.noreply.github.com>
2024-08-09 10:08:29 +0900

    feat: [2024.08.09] 연습

--- level1/체육복.js
+++ level1/체육복.js
@@ -1,24 +1,15 @@
 
 function solution(n, lost, reserve) {
-    for(let i in lost){
-        let lostNum = lost[i];
-        for(let j in reserve){
-            let reserveNum = reserve[j];
-            if(lostNum == reserveNum){
-                reserve.splice(j,1);
-                lost.splice(i,1);
-            }
-        }
-    }
-    console.log(reserve +  " " + lost)
 
-    for(let i in lost){
+    let actualReserve  = reserve.filter(r => !lost.includes(r)).sort();
+    let actualLost  = lost.filter(l => !reserve.includes(l)).sort();
+    for (let i = 0; i < actualLost.length; i++) {
         let rslt = false;
-        let lostNum = lost[i];
-        for(let j in reserve){
-            let reserveNum = reserve[j];
+        let lostNum = actualLost[i];
+        for(let j in actualReserve){
+            let reserveNum = actualReserve[j];
             if((!rslt && (lostNum == reserveNum + 1 || lostNum == reserveNum - 1))){
-                reserve.splice(j,1);
+                actualReserve.splice(j,1);
                rslt = true;
             }
         }
@@ -27,9 +18,11 @@ function solution(n, lost, reserve) {
     return n;
 }
 
-// console.log(solution(5,[2,4],[1,3,5]));
-// console.log(solution(5,[2,4],[3]));
-// console.log(solution(3,[3],[1]));
-// console.log(solution( 5, [4, 5], [3,4]));
-// console.log(solution(  5, [1, 2], [2,3]));
+
+console.log(solution(5,[2,4],[1,3,5]));
+console.log(solution(5,[2,4],[3]));
+console.log(solution(3,[3],[1]));
+console.log(solution( 5, [4, 5], [3,4]));
+console.log(solution(  5, [1, 2], [2,3]));
 console.log(solution(  5, [3, 4], [4, 3]));
+console.log(solution(  5, [4, 2], [3, 5])); //5
